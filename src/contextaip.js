31e5f55cb7e2b92d1d9ceccc0727b9202dcbe465
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2024-10-21 19:43:39 +0330

    Update contextaip.js

--- src/contextaip.js
+++ src/contextaip.js
@@ -1,15 +1,28 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
+import cartItems from "./data"
+import Reducer from "./reducer";
 
-const Themecon = createContext();
-export const ThemeProvider=({children})=>{
-    const [dark , setdark]=useState(false)
+const CartContext = createContext();
 
-    return( <Themecon.Provider value={{dark,setdark}}>
-        {children}
-        </Themecon.Provider>
-    )
-
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+  };
+const CartProvider = ({ children }) => {
+    const [state , disp]= useReducer(Reducer,initialState);
+    const clearCart=()=>{
+        disp({type:'CLEAR_CART'});
     }
-export const UseethemeContext=()=>{
-    return (useContext( Themecon) )
+    const Removeid= id =>{
+        disp({type:'REMOVE',payload:id});
+    } 
+    return(
+        <CartContext.Provider value={{...state,clearCart,Removeid}}>{children}</CartContext.Provider>
+    )
 }
+const useCartContext = ()=>{
+    return useContext(CartContext);
+}
+export {CartProvider, useCartContext};
